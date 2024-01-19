6c432aec8658fc59157cec0f3fbd3c7b6e9e19d0
AnaGamplay <anaclarasilvestre360@gmail.com>
2024-01-19 15:39:27 -0400

    BD automatico com Alchemy

--- app.py
+++ app.py
@@ -3,10 +3,18 @@ from flask_restful import Api
 from resources.hotel import Hoteis, Hotel
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
+app.config['SQLALCHEMT_TRACK_MODIFICATIONS'] = False
 api = Api(app)
     
 api.add_resource(Hoteis, '/hoteis')
 api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
 
 if __name__ == '__main__':
+    from sql_alchemy import banco
+    banco.init_app(app)
     app.run(debug=True)
+
+    # Antes da primeira requisição verifica se existe um banco e executa o create all se não existir
+    with app.app_context():
+        banco.create_all()
