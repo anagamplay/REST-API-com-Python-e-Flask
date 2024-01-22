cba1458fefc0b2d70ca1bdb53392008050b9cbe0
AnaGamplay <anaclarasilvestre360@gmail.com>
2024-01-22 16:47:19 -0400

    cadastro de usuarios

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 from resources.hotel import Hoteis, Hotel
-from resources.usuario import User
+from resources.usuario import User, UserRegister
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
@@ -10,12 +10,13 @@ api = Api(app)
     
 api.add_resource(Hoteis, '/hoteis')
 api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
-api.add_resource(User,'/usuarios/<int: user_id>')
+api.add_resource(User,'/usuarios/<int:user_id>')
+api.add_resource(UserRegister, '/cadastro')
 
 if __name__ == '__main__':
     from sql_alchemy import banco
     banco.init_app(app)
     app.run(debug=True)
-    
+
     with app.app_context():
         banco.create_all()
--- resources/usuario.py
+++ resources/usuario.py
@@ -2,7 +2,7 @@ from flask_restful import Resource, reqparse
 from models.usuario import UserModel
     
 class User(Resource):
-
+    # /usuarios/{user_id}
     def get(self, user_id):
         user = UserModel.find_user(user_id)
         if user:
@@ -13,8 +13,23 @@ class User(Resource):
         user = UserModel.find_user(user_id)
         if user:
             try:
-                user.delete_hotel()
+                user.delete_user()
             except:
-                return {'message': 'An error ocurred trying to delete hotel.'}, 500
-            return {'message': 'Hotel deleted.'}
-        return {'message': 'Hotel not found.'}, 404
+                return {'message': 'An error ocurred trying to delete user.'}, 500
+            return {'message': 'User deleted.'}
+        return {'message': 'User not found.'}, 404
+    
+class UserRegister(Resource):
+    # /cadatro
+    def post(post):
+        atributos = reqparse.RequestParser()
+        atributos.add_argument('login', type=str, required=True, help="The field 'login' cannot left blank")
+        atributos.add_argument('senha', type=str, required=True, help="The field 'senha' cannot left blank")
+        dados = atributos.parse_args()
+
+        if UserModel.find_by_login(dados['login']):
+            return {'message': "The login '{}' already exists.".format(dados['login'])}
+        
+        user = UserModel(**dados)
+        user.save_user()
+        return {'message': 'User created successfully!'}, 201
