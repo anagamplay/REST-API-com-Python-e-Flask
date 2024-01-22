fdcf15320ab20dc2f484ef591f3bb515c9506208
AnaGamplay <anaclarasilvestre360@gmail.com>
2024-01-22 14:13:34 -0400

    hotel

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,30 +1,6 @@
 from flask_restful import Resource, reqparse
 from models.hotel import HotelModel
 
-hoteis = [
-    {
-    'hotel_id': 'alpha',
-    'nome': 'Alpha Hotel',
-    'estrelas': 4.3,
-    'diaria': 420.34,
-    'cidade': 'Rio de Janeiro'
-    },
-    {
-    'hotel_id': 'bravo',
-    'nome': 'Bravo Hotel',
-    'estrelas': 4.4,
-    'diaria': 380.34,
-    'cidade': 'Santa Catarina'
-    },
-    {
-    'hotel_id': 'chalie',
-    'nome': 'Charlie Hotel',
-    'estrelas': 3.9,
-    'diaria': 320.34,
-    'cidade': 'Santa Catarina'
-    }
-]
-
 class Hoteis(Resource):
     def get(self):
         return {'hoteis': [hotel.json() for hotel in HotelModel.query.all()]}
