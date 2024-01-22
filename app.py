fb723f376cf2d642a4aedb478220aa22e259351f
AnaGamplay <anaclarasilvestre360@gmail.com>
2024-01-22 14:21:53 -0400

    get de delete de usuario

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 from resources.hotel import Hoteis, Hotel
+from resources.usuario import User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
@@ -9,12 +10,12 @@ api = Api(app)
     
 api.add_resource(Hoteis, '/hoteis')
 api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
+api.add_resource(User,'/usuarios/<int: user_id>')
 
 if __name__ == '__main__':
     from sql_alchemy import banco
     banco.init_app(app)
     app.run(debug=True)
-
-    # Antes da primeira requisição verifica se existe um banco e executa o create all se não existir
+    
     with app.app_context():
         banco.create_all()
