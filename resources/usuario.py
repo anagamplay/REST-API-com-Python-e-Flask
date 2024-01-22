f8da95355eeeac5e39599b306854c13e53efabda
AnaGamplay <anaclarasilvestre360@gmail.com>
2024-01-22 14:11:48 -0400

    resolving conflicts

--- resources/usuario.py
+++ resources/usuario.py
@@ -2,7 +2,6 @@ from flask_restful import Resource, reqparse
 from models.usuario import UserModel
     
 class User(Resource):
-    # /usuarios/{user_id}
     def get(self, user_id):
         user = UserModel.find_user(user_id)
         if user:
