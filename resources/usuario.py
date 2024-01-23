a56e738e4626394e983ab9b7b0518509319577a9
AnaGamplay <anaclarasilvestre360@gmail.com>
2024-01-23 10:52:22 -0400

    login de usuario

--- resources/usuario.py
+++ resources/usuario.py
@@ -1,5 +1,11 @@
 from flask_restful import Resource, reqparse
 from models.usuario import UserModel
+from flask_jwt_extended import create_access_token
+from secrets import compare_digest
+
+atributos = reqparse.RequestParser()
+atributos.add_argument('login', type=str, required=True, help="The field 'login' cannot left blank")
+atributos.add_argument('senha', type=str, required=True, help="The field 'senha' cannot left blank")
     
 class User(Resource):
     def get(self, user_id):
@@ -21,9 +27,6 @@ class User(Resource):
 class UserRegister(Resource):
     # /cadatro
     def post(post):
-        atributos = reqparse.RequestParser()
-        atributos.add_argument('login', type=str, required=True, help="The field 'login' cannot left blank")
-        atributos.add_argument('senha', type=str, required=True, help="The field 'senha' cannot left blank")
         dados = atributos.parse_args()
 
         if UserModel.find_by_login(dados['login']):
@@ -32,3 +35,15 @@ class UserRegister(Resource):
         user = UserModel(**dados)
         user.save_user()
         return {'message': 'User created successfully!'}, 201
+    
+class UserLogin(Resource):
+
+    @classmethod
+    def post(cls):
+        dados = atributos.parse_args()
+        user = UserModel.find_by_login(dados['login'])
+
+        if user and compare_digest(user.senha, dados['senha']):
+            token_de_acesso = create_access_token(identity=user.user_id)
+            return {'access_token': token_de_acesso}, 200
+        return {'message': 'The Username or password is incorrect.'}, 401 # Unauthorized
