26d49a4b3f7da5eb0c597f2026689383292ebf72
AnaGamplay <anaclarasilvestre360@gmail.com>
2024-01-23 14:00:57 -0400

    conexao com o banco e consulta sql

--- resources/hotel.py
+++ resources/hotel.py
@@ -41,9 +41,19 @@ path_params.add_argument('offset', type=float)
 
 class Hoteis(Resource):
     def get(self):
+        connection = sqlite3.connect('banco.db')
+        cursor = connection.cursor()
 
         dados = path_params.parse_args()
         dados_validos = {chave:dados[chave] for chave in dados if dados[chave] is not None}
+        parametros = normalize_path_params(**dados_validos)
+
+        if parametros.get('cidade'):
+            consulta = "SELECT * FROM hoteis \
+                WHERE (estrelas > ? and estrelas < ?) \
+                and (diaria > ? and diaria < ?) \
+                LIMIT ? OFFSET ?"
+
         return {'hoteis': [hotel.json() for hotel in HotelModel.query.all()]}
     
 class Hotel(Resource):
