bd9decf435b330e53995b876f9fb8333c1727e28
AnaGamplay <anaclarasilvestre360@gmail.com>
2024-02-03 18:09:46 -0400

    correção

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ api.add_resource(UserLogin, '/login')
 api.add_resource(UserLogout, '/logout')
 api.add_resource(Sites, '/sites')
 api.add_resource(Site, '/sites/<string:url>')
-api.add_resource(UserConfirm, '/confimacao/<int:user_id>')
+api.add_resource(UserConfirm, '/confirmacao/<int:user_id>')
 
 if __name__ == '__main__':
     from sql_alchemy import banco
