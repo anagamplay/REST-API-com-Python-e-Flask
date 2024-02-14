b672a3b2b795e6fad0088aca39ed3618ef36c87e
Ana-Ubuntu <anaclarasilvestre360@gmail.com>
2024-02-14 17:18:16 -0400

    implementando funcao de envio de emails de confirmacao

--- models/usuario.py
+++ models/usuario.py
@@ -1,5 +1,11 @@
 from sql_alchemy import banco
 from flask import request, url_for
+from requests import post
+
+MAILGUN_DOMAIN = 'sandbox1fb8f6fb8e4240e5bfabf019dc8bc6e1.mailgun.org'
+MAILGUN_API_KEY = '0411ea0bc2bbc95806ebe1da599b9778-8c8e5529-c35baef7'
+ROM_TITLE = 'NO-REPLY'
+FROM_EMAIL = 'no-reply@restapi.com'
 
 class UserModel(banco.Model):
     __tablename__ = 'usuarios'
@@ -18,6 +24,17 @@ class UserModel(banco.Model):
     
     def send_confirmation_email(self):
         link = request.url_root[:-1]  + url_for('userconfirm', user_id=self.user_id) 
+        return post('https://api.mailgun.net/v3/{}/messages'.format(MAILGUN_DOMAIN),
+                    auth=('api', MAILGUN_API_KEY),
+                    data={'from': '{} <{}>'.format(FROM_TITLE, FROM_EMAIL),
+                        'to': self.email,
+                        'subject': 'Confirmação de Cadastro',
+                        'text': 'Confirme seu cadastro clicando no link a seguir: {}'.format(link),
+                        'html': '<html><p>\
+                            Confirme seu cadastro clicando no link a seguir: <a href="{}">CONFIRMAR EMAIL</a>\
+                            </p></html>'.format(link)
+                        }
+                )
 
     def json(self):
         return {
